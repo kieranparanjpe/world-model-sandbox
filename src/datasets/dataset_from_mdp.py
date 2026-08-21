937b0169d481fad27d1146681a35c55daf145729
Kieran Paranjpe <kieran.paranjpe@autodesk.com>
2026-08-21 14:58:45 -0400

    follow policy in dataset sa creation

--- src/datasets/dataset_from_mdp.py
+++ src/datasets/dataset_from_mdp.py
@@ -1,8 +1,10 @@
 import argparse
 from datetime import datetime
+from typing import Optional
 
 import torch.distributions
 from rl_commons.mdp import Mdp, MdpTerminationState, MdpGym, MdpConfig
+from rl_commons.policies import Policy
 from tqdm.auto import tqdm
 from pathlib import Path
 import torch
@@ -12,13 +14,18 @@ from src.mdp.utils import get_random_policy
 
 class DatasetFromMDP:
 
-    def __init__(self, mdp : Mdp, timesteps : int, mdp_id : str):
+    def __init__(self, mdp : Mdp, timesteps : int, mdp_id : str, policy_id_path: Optional[tuple[str, str]] = None):
         self._mdp = mdp
         self._timesteps = timesteps
         time = f"{datetime.now():%Y-%m-%d-%H-%M-%S}"
         self._output_path = Path(__file__).resolve().parents[2] / "datasets" / mdp_id / "state-action" / (f"dataset"
                                                                                                       f"_{time}.pt")
-
+        self._policy = Policy.load(policy_id_path[1],
+                                  map_location=self._mdp.device,
+                                  obs_dimension=self._mdp.obs_dimension,
+                                  action_dimension=self._mdp.action_dimension,
+                                  policy_id=policy_id_path[0]) \
+            if policy_id_path else get_random_policy(self._mdp)
         self._policy = get_random_policy(self._mdp)
 
         self._dataset = {
@@ -27,13 +34,17 @@ class DatasetFromMDP:
             "next_observations": torch.zeros((timesteps, self._mdp.obs_dimension), dtype=torch.float32,
                                                  device=self._mdp.device),
             "actions": torch.zeros((timesteps, self._mdp.action_dimension), dtype=torch.float32,
-                                                 device=self._mdp.device)
+                                                 device=self._mdp.device),
+            "episode": torch.zeros((timesteps, 1), dtype=torch.int64,
+                                   device=self._mdp.device)
         }
 
     def collect(self):
         last_observation = self._mdp.reset()
 
         action_sampler = self._policy.forward(last_observation)
+
+        episode = torch.tensor(0, device=self._mdp.device)
 
         with torch.no_grad():
             for timestep in tqdm(range(self._timesteps)):
@@ -46,9 +57,12 @@ class DatasetFromMDP:
                 self._dataset["current_observations"][timestep] = last_observation
                 self._dataset["next_observations"][timestep] = next_observation
                 self._dataset["actions"][timestep] = action
+                self._dataset["episode"][timestep] = episode
 
                 if termination_state is not MdpTerminationState.IN_PROGRESS:
+                    episode += 1
                     last_observation = self._mdp.reset()
+
                 else:
                     last_observation = next_observation
 
@@ -64,6 +78,8 @@ def parse_args():
 
     parser.add_argument("--environment", "-e", help="Environment Id to run", default="CartPole-v1")
     parser.add_argument("--timesteps", "-t", help="Number of samples to collect", default=10000)
+    parser.add_argument("--weights", "-w", help="Path to policy", default=None)
+    parser.add_argument("--policy", "-p", help="Policy type", default=None)
 
     return parser.parse_args()
 
@@ -74,7 +90,10 @@ def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     mdp = MdpGym(args.environment, device, mdp_config=MdpConfig(normalise_obs=False, normalise_reward=False))
 
-    dataset_from_mdp = DatasetFromMDP(mdp, int(args.timesteps), args.environment)
+    policy_id_path = (args.policy, args.weights) if args.policy and args.weights else None
+
+
+    dataset_from_mdp = DatasetFromMDP(mdp, int(args.timesteps), args.environment, policy_id_path=policy_id_path)
     dataset_from_mdp.collect()
     dataset_from_mdp.save()
 
